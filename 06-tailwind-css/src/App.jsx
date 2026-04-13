b5c4f556c2b9888b3a4e5b48a554522b2ebf3755
anuj.raj <anuj.raj@decimal.co.in>
2026-04-13 18:26:27 +0530

    changes for tailwind css

---
+++ 06-tailwind-css/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-gray-900 h-screen w-full'>
+      <div className='h-1/12 bg-gray-700 p-10 w-20'>Card</div>
+    </div>
+  )
+}
+
+export default App
